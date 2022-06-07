d77861da153f0cb50fbfff0bde0c0099f09c7d96
devrogerzen <devrogerzen@gmail.com>
2022-06-07 02:54:33 -0300

    up project

---
+++ README.md
@@ -0,0 +1,3 @@
+# React.js Redux-Toolkit / Axios
+## Link Demo:
+![Capture del proyecto](https://github.com/rogersaya26/ConsumeApiState-Reqres.in-Redux-Toolkit/blob/master/src/images/ReduxReqres.PNG)
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-dark bg-dark'>
+      <div className='container'>
+        <a className='navbar-brand fs-2' href="/">React.js -Hooks -Redux-Toolkits -Axios / API Reqres.in -Bootstrap</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
